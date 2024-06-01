f6a05743656eb0822abf747dccae381ace49dd88
injana <switzeree03@gmail.com>
2024-06-02 01:11:04 +0900

    trylang

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Document</title>
+</head>
+<body>
+ <nav class="navbar navbar-expand-sm fixed-top">
+    <a href="" class="navbar-brand">
+    <img src="picture/try.jpg" alt="Logo" class="logo-img">
+    <span>P</span>et For All</a>
+    <div>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="" class="nav-link">Home</a>
+             </li>
+             <li class="nav-item"><a href="" class="nav-link">Adapt</a>
+             </li>
+             <li class="nav-item"><a href="" class="nav-link">Donate</a>
+             </li>
+             <li class="nav-item"><a href="" class="nav-link">Volunteer</a>
+             </li>
+             <li class="nav-item"><a href="" class="nav-link">How to take care</a>
+             </li>
+             <li class="nav-item"><a href="" class="nav-link">About Us</a>
+             </li>
+             <li class="nav-item"><a href="" class="nav-link">Log in</a>
+             </li>
+             <li class="nav-item"><a href="" class="nav-link">Sign up</a>
+             </li> 
+        </ul>
+    </div>
+</nav>
+<!-- Header-->
+<section>
+    <div class="header">
+        <div>
+            <div class="img">
+
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="picture/bg.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="picture/3.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="picture/2.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+
+
+            </div>
+            
+            <div class="Overlay">
+            
+          <h1 style="color: white; margin-top: 30vh; margin-left: 10vh;">Not Only People <br>Need a House</h1>
+          <p style="color: white; margin-left: 10vh;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat quis <br> eaque eligendi nobis, sapiente id adipisci velit, architecto quam eveniet, ab cumque? Excepturi ratione <br> possimus repellendus eligendi nihil dicta ullam.</p>
+    
+</div>
+            
+            </div>   
+    </div>
+</section> 
+
+
+
+<!-- Script for JQuery -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Script for Bootsrtrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
