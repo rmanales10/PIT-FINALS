02d810fe0f4397b9cad0f668022faef634150549
elsiemry <switzeree03@gmail.com>
2024-06-16 01:39:52 +0900

    hehe

---
+++ user/care1.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="retro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/ecd5e25db3.js" crossorigin="anonymous"></script>
+    
+    
+
+    <title>The Basic Necessities of Proper Pet Care</title>
+</head>
+<body class="font-serif">
+    <nav class="navbar fixed top-0 flex items-center justify-between w-ful text-white p-5">
+        <div class="flex items-center">
+            <a href="takecare">
+                <img src="../picture/logo.png" alt="Logo" class="ml-10" width="110px">
+            </a>
+            <a href="takecare" class="text-5xl">Pet For All</a>
+        </div>
+  
+    </nav>
+  
+
+
+    <div class="bg-#F8F4E1">
+        <div class="bg-no-repeat bg-top bg-fixed h-screen w-full" style="background-image: url('../picture/carebg1.jpg'); background-size: 85% auto;">
+            <div class="absolute inset-0 flex items-center justify-center">
+                <div class="bg-black bg-opacity-50 text-white p-8 rounded-md mt-[50vh]">
+                    <h1 class="text-[69.6px]">
+                        The Basic Necessities of Proper Pet Care
+                    </h1>
+                </div>
+            </div>
+        </div>
+
+
+
+        <section class=" mb-[20vh]">
+        <div class="flex flex-col text-center text-[35px] mt-11" style="font-weight: bold;">
+            <h1>
+                Proper pet care is not just a responsibility—it's a way to show love <br> and ensure the well-being of our beloved furry companions. Understanding<br> and fulfilling the basic necessities of pet care is essential for creating a<br> happy and healthy environment for our pets.
+            </h1>
+        </div>
+
+        <div class="mt-24 ml-5 text-pretty flex flex-col gap-5">
+            
+    <h3 class="text-[5vh]" style="line-height: 10px;"> <b>Nutrition:</b>
+    <span class="text-xl"> A well-balanced and nutritious diet is the foundation of good health for pets. Consult with your veterinarian to determine the best food options based on your pet's breed, age, and specific dietary needs. Fresh water should always be readily available.</span></h3>
+
+    <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Exercise:</b>
+        <span class="text-xl"> Regular physical activity is crucial for keeping pets active and maintaining their overall health. Whether it's daily walks, playtime, or interactive toys, engaging in exercise tailored to your pet's energy level is key to preventing obesity and promoting vitality.</span></h3>
+
+     <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Grooming:</b>
+        <span class="text-xl"> Regular grooming sessions are more than just about aesthetics—they are essential for maintaining your pet's hygiene and health. Brushing their fur, trimming nails, and cleaning ears and teeth are all part of a good grooming routine that also strengthens the bond between you and your pet.</span></h3>
+
+    <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Veterinary Care:</b>
+        <span class="text-xl"> Routine visits to the veterinarian are vital for preventive care and early detection of any health issues. Vaccinations, check-ups, and necessary treatments should be scheduled regularly to ensure your pet's well-being and longevity.</span></h3>
+    
+    <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Safe Environment:</b>
+        <span class="text-xl"> Creating a safe living space for your pet is crucial. Remove any potential hazards, secure fences and gates, and provide a warm and comfortable shelter. A safe environment is essential for your pet's physical and emotional well-being.</span></h3>
+                    
+    <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Mental Stimulation:</b>
+         <span class="text-xl"> Pets need mental stimulation as much as physical exercise. Engage them with puzzle toys, interactive games, or training sessions to keep their minds sharp and prevent boredom or behavioral issues.
+        </span></h3>
+    
+    <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Socialization:</b>
+    <span class="text-xl">Exposing pets to various environments, people, and animals from an early age helps them develop social skills and positive behavior. Proper socialization is key to a well-adjusted and happy pet.
+    </span></h3>
+
+    <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Identification:</b>
+    <span class="text-xl">Ensure your pet wears a collar with identification tags and consider microchipping for permanent identification. This helps increase the chances of a safe return if your pet ever gets lost.
+        </span></h3>
+
+    <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Stress Reduction:</b>
+    <span class="text-xl">Create a calm and stress-free environment for your pet by providing a quiet space for rest and relaxation. Avoid loud noises or sudden changes that may cause anxiety or fear.
+            </span></h3>
+
+       <h3 class="text-[5vh] mt-[50px]" style="line-height: 10px;"> <b>Love and Attention:</b>
+        <span class="text-xl">Above all, shower your pet with love and attention. Spend quality time together, offer praise and affection, and build a strong bond. Your pet's emotional well-being is just as important as their physical health.
+                </span></h3>
+
+                <p class="text-l mt-10"> <b>By, prioritizing these basic necessities of proper pet care you can ensure that your furry friend leads a happy, healthy, and fullfiling life. Remember, being a responsible pet owner means providing the care and attention that your pet deserves.
+                </p></b>
+    
+
+
+    </section>
+
+
+
+
+
+
+
+
+      <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+      integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  
+    <script>
+      $(document).scroll(function () {
+        $(".navbar").toggleClass("scroll", $(this).scrollTop() > $(
+          ".navbar").height())
+  
+      });
+      </script>
+  
+      <style>
+.navbar {
+    padding: 5px;
+    display: flex;
+    justify-content: space-between;
+    transition: 0.3s;
+    font-size: 18px;
+    z-index: 50;
+}
+
+  .navbar.scroll{
+      padding: 5px;
+      background-color: #b08f6f;
+  
+  }
+  html::-webkit-scrollbar {
+      width: 10px;
+  }
+  html::-webkit-scrollbar-track{
+      background-color: #333;
+  }
+  html::-webkit-scrollbar-thumb{
+      background:#AF8F6F;
+  }
+      </style>
+    
+</body>
+</html>
