634f0dd37f825a3710489713c2fe8d52b63553a7
Shirly Uy <162430149+shing2x@users.noreply.github.com>
2024-06-16 01:41:06 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -7,7 +7,7 @@ $dbname = "pet";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check connections
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
